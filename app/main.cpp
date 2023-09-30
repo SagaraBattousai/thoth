c21155bcb21c6107be886116909ecadab15d5006
James Calo <jamesafcalo@gmail.com>
2023-09-30 12:26:42 +0100

    Still cannot work out what gcc's problem is

--- app/main.cpp
+++ app/main.cpp
@@ -5,6 +5,7 @@
 #include <numeric>
 #include <iostream>
 #include <iterator>
+#include <type_traits>
 
 #include <thoth/matrix.h>
 
@@ -14,7 +15,7 @@ int main() {
   std::vector<int> strides(1);
 
 
-  for (int i = (int)strides.size() - 1; i >= 0; --i)
+  for (auto i = static_cast<std::make_signed_t<std::vector<int>::size_type>>(strides.size() - 1); i >= 0; --i)
   {
     std::cout << vec[i] << ", " << strides[i] << " : ";
   }
